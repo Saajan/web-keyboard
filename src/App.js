985894192c69f2ce90e424732a1b3f182397bc58
Saajan <saajan.sn@gmail.com>
2019-04-10 02:21:26 +0530

    layout changes to piano

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 
 import React, { Component } from 'react';
-import {
-  Slider, Select, Row, Col, InputNumber
-} from 'antd';
+import { Slider, Select, InputNumber } from 'antd';
 
 import { createNoteTable } from './helpers/midi';
 
 import OctaveKey from './components/octavekeys';
 
 import 'antd/dist/antd.css';
-import './App.css';
+import './App.scss';
 
 const Option = Select.Option;
 
@@ -43,46 +41,40 @@ class App extends Component {
   render() {
     const { volume, type } = this.state;
     return (
-      <div>
+      <div className="page">
+        <div className="settings-bar">
+          <div className="volume-container">
+            <span>Volume: </span>
+            <Slider
+              min={0}
+              max={1}
+              step={0.01}
+              onChange={this.onChange}
+              value={typeof volume === 'number' ? volume : 0}
+            />
+            <InputNumber
+              min={0}
+              max={1}
+              step={0.01}
+              style={{ marginLeft: 16 }}
+              value={volume}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className="wave-container">
+            <span>Current waveform: </span>
+            <Select name="waveform" defaultValue="square" style={{ width: 120 }} onChange={this.handleChange}>
+              <Option value="sine">Sine</Option>
+              <Option value="square">Square</Option>
+              <Option value="sawtooth">Sawtooth</Option>
+              <Option value="triangle">Triangle</Option>
+            </Select>
+          </div>
+        </div>
         <div className="container">
           {this.state.keys.map((keys, index) => {
             return <OctaveKey key={"octave" + index} keys={keys} index={index} type={type} volume={volume}></OctaveKey>
           })}
-        </div>
-        <div className="settingsBar">
-          <Row>
-            <Col span={12}>
-              <span>Volume: </span>
-              <Slider
-                min={0}
-                max={1}
-                step={0.01}
-                onChange={this.onChange}
-                value={typeof volume === 'number' ? volume : 0}
-              />
-            </Col>
-            <Col span={4}>
-              <InputNumber
-                min={0}
-                max={1}
-                step={0.01}
-                style={{ marginLeft: 16 }}
-                value={volume}
-                onChange={this.onChange}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col span={12}>
-              <span>Current waveform: </span>
-              <Select name="waveform" defaultValue="square" style={{ width: 120 }} onChange={this.handleChange}>
-                <Option value="sine">Sine</Option>
-                <Option value="square">Square</Option>
-                <Option value="sawtooth">Sawtooth</Option>
-                <Option value="triangle">Triangle</Option>
-              </Select>
-            </Col>
-          </Row>
         </div>
       </div>
     );
